4d170caf3f083d44d79430101e383cfa65ca046e
John Marks <john@johnmarks.co.uk>
2014-02-06 12:35:26 +0000

    Added Fody

---
+++ App/src/Adaptive.ReactiveTrader.Client/UI/Shell/ShellView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Adaptive.ReactiveTrader.Client.UI.Shell
+{
+    public partial class ShellView
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
