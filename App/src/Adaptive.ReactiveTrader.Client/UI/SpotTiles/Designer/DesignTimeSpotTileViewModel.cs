3486bbe1385f10260234eb0d5cad712f3c1c7659
odeheurles <mail@odeheurles.com>
2014-02-22 19:11:58 +0000

    More styling

---
+++ App/src/Adaptive.ReactiveTrader.Client/UI/SpotTiles/Designer/DesignTimeSpotTileViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Adaptive.ReactiveTrader.Client.Domain.Models;
+using Adaptive.ReactiveTrader.Shared.UI;
+
+namespace Adaptive.ReactiveTrader.Client.UI.SpotTiles.Designer
+{
+    public class DesignTimeSpotTileViewModel : ViewModelBase, ISpotTileViewModel
+    {
+        public DesignTimeSpotTileViewModel()
+        {
+            Pricing = new DesignTimeSpotTilePricingViewModel();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ISpotTilePricingViewModel Pricing { get; private set; }
+        public ISpotTileAffirmationViewModel Affirmation { get; private set; }
+        public bool ShowingAffirmation { get { return false; } }
+        public void OnTrade(ITrade trade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DismissAffirmation()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
