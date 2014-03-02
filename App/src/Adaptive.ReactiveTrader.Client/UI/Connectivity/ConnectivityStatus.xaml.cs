bd4c62eae2b29dfda8a45b42ec53094287b9ee44
Olivier Deheurles <mail@odeheurles.com>
2014-03-02 23:10:47 +0000

    Add a link to Adaptive web site in the status bar (fixed #31)

--- App/src/Adaptive.ReactiveTrader.Client/UI/Connectivity/ConnectivityStatus.xaml.cs
+++ App/src/Adaptive.ReactiveTrader.Client/UI/Connectivity/ConnectivityStatus.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace Adaptive.ReactiveTrader.Client.UI.Connectivity
+﻿using System.Diagnostics;
+using System.Windows.Navigation;
+
+namespace Adaptive.ReactiveTrader.Client.UI.Connectivity
 {
     public partial class ConnectivityStatusView
     {
         public ConnectivityStatusView()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start("http://www.weareadaptive.com");
         }
     }
 }
