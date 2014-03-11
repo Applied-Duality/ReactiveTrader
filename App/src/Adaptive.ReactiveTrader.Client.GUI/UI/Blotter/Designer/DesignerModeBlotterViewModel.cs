79795e5ab29b73381e16ac6844436b6679ae6eba
Olivier Deheurles <mail@odeheurles.com>
2014-03-11 17:06:53 +0000

    fix #37

--- App/src/Adaptive.ReactiveTrader.Client.GUI/UI/Blotter/Designer/DesignerModeBlotterViewModel.cs
+++ App/src/Adaptive.ReactiveTrader.Client.GUI/UI/Blotter/Designer/DesignerModeBlotterViewModel.cs
@@ -38,6 +38,19 @@ namespace Adaptive.ReactiveTrader.Client.UI.Blotter.Designer
                 TradeStatus = "Done",
                 TraderName = "Olivier"
             });
+
+            Trades.Add(new DesignerModeTradeViewModel
+            {
+                CurrencyPair = "EUR / USD",
+                Direction = Direction.BUY,
+                Notional = "1,000,000 EUR",
+                SpotRate = 1.23456m,
+                TradeDate = DateTime.Now,
+                ValueDate = DateTime.Now.AddDays(2).AddSeconds(1),
+                TradeId = "12834",
+                TradeStatus = "Done",
+                TraderName = "Olivier"
+            });
         }
     }
 }
