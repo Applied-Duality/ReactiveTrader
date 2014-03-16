418e4d495ec47bdc7fb22d9e43b07029d894d5a9
Olivier Deheurles <mail@odeheurles.com>
2014-03-16 23:12:19 +0000

    app now use reactive trader API

--- App/src/Adaptive.ReactiveTrader.Client.JS/app.ts
+++ App/src/Adaptive.ReactiveTrader.Client.JS/app.ts
@@ -1,35 +1,45 @@
-﻿/// <reference path="typings/signalr/signalr.d.ts" />
-/// <reference path="typings/rx.js/rx.d.ts"/>
-/// <reference path="Transport/Connection.ts" />
-/// <reference path="ServiceClients/ReferenceDataServiceClient.ts" />
-
-window.onload = () => {
+﻿window.onload = () => {
     var el = document.getElementById('content');
 
-    
-    var connection = new Connection("http://localhost:8080", "Olivier");
+    var reactiveTrader: IReactiveTrader = new ReactiveTrader();
 
-    connection.status
-        .subscribe(
-            status => console.log("Connection status changed to " + status),
-            ex => console.log(ex));
+    reactiveTrader
+        .initialize("olivier", "http://localhost:800")
+        .subscribe(_=> {
+            reactiveTrader.connectionStatusStream.subscribe(s=> console.log("Connection status: " + s));
 
-    connection
-        .initialize()
-        .subscribe(
-            _=> {
-                console.log("Connected");
-                var refData = new ReferenceDataServiceClient(connection);
-                refData.getCurrencyPairUpdates()
-                    .subscribe(
-                        currencyPairs=> console.log(currencyPairs),
-                        ex=> console.error(ex));
+            reactiveTrader.referenceDataRepository.getCurrencyPairs()
+                .select(currencyPairs=> currencyPairs[0])
+                .selectMany(currencyPairUpdate=> currencyPairUpdate.currencyPair.prices)
+                .select((price: IPrice)=> price.bid.rate + " / " + price.ask.rate)
+                .subscribe(p=> console.log(p));
+        },
+            ex=> console.error(ex));
 
-                var pricing = new PricingServiceClient(connection);
-                pricing.getSpotStream("EURUSD")
-                    .subscribe(
-                        (price: PriceDto)=> console.log(price.Bid + "/" + price.Ask),
-                        ex=> console.error(ex));
-            },
-            ex=> console.log(ex));
+
+    //var connection = new Connection("http://localhost:8080", "Olivier");
+
+    //connection.status
+    //    .subscribe(
+    //        status => console.log("Connection status changed to " + status),
+    //        ex => console.log(ex));
+
+    //connection
+    //    .initialize()
+    //    .subscribe(
+    //        _=> {
+    //            console.log("Connected");
+    //            var refData = new ReferenceDataServiceClient(connection);
+    //            refData.getCurrencyPairUpdates()
+    //                .subscribe(
+    //                    currencyPairs=> console.log(currencyPairs),
+    //                    ex=> console.error(ex));
+
+    //            var pricing = new PricingServiceClient(connection);
+    //            pricing.getSpotStream("EURUSD")
+    //                .subscribe(
+    //                    (price: PriceDto)=> console.log(price.Bid + "/" + price.Ask),
+    //                    ex=> console.error(ex));
+    //        },
+    //        ex=> console.log(ex));
 };
--- App/src/Adaptive.ReactiveTrader.Client.JS/reactiveTrader.js
+++ App/src/Adaptive.ReactiveTrader.Client.JS/reactiveTrader.js
@@ -1,212 +1,47 @@
-﻿var Connection = (function () {
-    function Connection(address, username) {
-        var _this = this;
-        this._status = new Rx.Subject();
-        this._address = address;
-        this._hubConnection = $.hubConnection("http://localhost:8080/signalr");
-
-        // TODO set username header
-        this._hubConnection.disconnected(function () {
-            return _this.changeStatus(5 /* Closed */);
-        }).connectionSlow(function () {
-            return _this.changeStatus(2 /* ConnectionSlow */);
-        }).reconnected(function () {
-            return _this.changeStatus(4 /* Reconnected */);
-        }).reconnecting(function () {
-            return _this.changeStatus(3 /* Reconnecting */);
-        }).error(function (error) {
-            return console.log(error);
-        });
-
-        this._referenceDataHubProxy = this._hubConnection.createHubProxy("ReferenceDataHub");
-        this._blotterHubProxy = this._hubConnection.createHubProxy("BlotterHub");
-        this._executionHubProxy = this._hubConnection.createHubProxy("ExecutionHub");
-        this._pricingHubProxy = this._hubConnection.createHubProxy("PricingHub");
-
-        this.installListeners();
-    }
-    Connection.prototype.initialize = function () {
-        var _this = this;
-        return Rx.Observable.create(function (observer) {
-            _this.changeStatus(0 /* Connecting */);
-
-            console.log("Connecting to " + _this._address + "...");
-            _this._hubConnection.start().done(function () {
-                _this.changeStatus(1 /* Connected */);
-                observer.onNext(true);
-                console.log("Connected to " + _this._address + ".");
-            }).fail(function () {
-                _this.changeStatus(5 /* Closed */);
-                var error = "An error occured when starting SignalR connection.";
-                console.log(error);
-                observer.onError(error);
-            });
-
-            return Rx.Disposable.create(function () {
-                console.log("Stoping connection...");
-                _this._hubConnection.stop();
-                console.log("Connection stopped.");
-            });
-        });
-        //.publish()
-        //.refCount();
-    };
-
-    Connection.prototype.changeStatus = function (newStatus) {
-        this._status.onNext(new ConnectionInfo(newStatus, this.address));
-    };
-
-    Object.defineProperty(Connection.prototype, "status", {
-        get: function () {
-            return this._status;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Connection.prototype, "address", {
-        get: function () {
-            return this._address;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Connection.prototype, "referenceDataHubProxy", {
-        get: function () {
-            return this._referenceDataHubProxy;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Connection.prototype, "pricingHubProxy", {
-        get: function () {
-            return this._pricingHubProxy;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Connection.prototype, "executionHubProxy", {
-        get: function () {
-            return this._executionHubProxy;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Connection.prototype, "blotterHubProxy", {
-        get: function () {
-            return this._blotterHubProxy;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Connection.prototype, "allPrices", {
-        get: function () {
-            return this._allPrices;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Connection.prototype, "currencyPairUpdates", {
-        get: function () {
-            return this._currencyPairUpdates;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Object.defineProperty(Connection.prototype, "allTrades", {
-        get: function () {
-            return this._allTrades;
-        },
-        enumerable: true,
-        configurable: true
-    });
-
-    Connection.prototype.installListeners = function () {
-        var _this = this;
-        this._allPrices = new Rx.Subject();
-        this._currencyPairUpdates = new Rx.Subject();
-
-        this._pricingHubProxy.on("OnNewPrice", function (price) {
-            return _this._allPrices.onNext(price);
-        });
-        this._referenceDataHubProxy.on("OnCurrencyPairUpdate", function (currencyPairs) {
-            return _this._currencyPairUpdates.onNext(currencyPairs);
-        });
-        this._blotterHubProxy.on("OnNewTrade", function (trades) {
-            return _this._allTrades.onNext(trades);
-        });
-    };
-    return Connection;
-})();
-var ReferenceDataServiceClient = (function () {
-    function ReferenceDataServiceClient(connection) {
-        this._connection = connection;
-    }
-    ReferenceDataServiceClient.prototype.getCurrencyPairUpdates = function () {
-        return this.getTradesForConnection(this._connection.referenceDataHubProxy);
-    };
-
-    ReferenceDataServiceClient.prototype.getTradesForConnection = function (referenceDataHubProxy) {
-        var _this = this;
-        return Rx.Observable.create(function (observer) {
-            var currencyPairUpdateSubscription = _this._connection.currencyPairUpdates.subscribe(function (currencyPairUpdate) {
-                return observer.onNext([currencyPairUpdate]);
-            });
-
-            console.log("Sending currency pair subscription...");
-
-            referenceDataHubProxy.invoke("GetCurrencyPairs").done(function (currencyPairs) {
-                observer.onNext(currencyPairs);
-                console.log("Subscribed to currency pairs and received " + currencyPairs.length + " currency pairs.");
-            }).fail(function (ex) {
-                return observer.onError(ex);
-            });
-
-            return currencyPairUpdateSubscription;
-        }).publish().refCount();
-    };
-    return ReferenceDataServiceClient;
-})();
-/// <reference path="typings/signalr/signalr.d.ts" />
-/// <reference path="typings/rx.js/rx.d.ts"/>
-/// <reference path="Transport/Connection.ts" />
-/// <reference path="ServiceClients/ReferenceDataServiceClient.ts" />
-window.onload = function () {
+﻿window.onload = function () {
     var el = document.getElementById('content');
 
-    var connection = new Connection("http://localhost:8080", "Olivier");
+    var reactiveTrader = new ReactiveTrader();
 
-    connection.status.subscribe(function (status) {
-        return console.log("Connection status changed to " + status);
-    }, function (ex) {
-        return console.log(ex);
-    });
-
-    connection.initialize().subscribe(function (_) {
-        console.log("Connected");
-        var refData = new ReferenceDataServiceClient(connection);
-        refData.getCurrencyPairUpdates().subscribe(function (currencyPairs) {
-            return console.log(currencyPairs);
-        }, function (ex) {
-            return console.error(ex);
+    reactiveTrader.initialize("olivier", "http://localhost:800").subscribe(function (_) {
+        reactiveTrader.connectionStatusStream.subscribe(function (s) {
+            return console.log("Connection status: " + s);
         });
 
-        var pricing = new PricingServiceClient(connection);
-        pricing.getSpotStream("EURUSD").subscribe(function (price) {
-            return console.log(price.Bid + "/" + price.Ask);
-        }, function (ex) {
-            return console.error(ex);
+        reactiveTrader.referenceDataRepository.getCurrencyPairs().select(function (currencyPairs) {
+            return currencyPairs[0];
+        }).selectMany(function (currencyPairUpdate) {
+            return currencyPairUpdate.currencyPair.prices;
+        }).select(function (price) {
+            return price.bid.rate + " / " + price.ask.rate;
+        }).subscribe(function (p) {
+            return console.log(p);
         });
     }, function (ex) {
-        return console.log(ex);
+        return console.error(ex);
     });
+    //var connection = new Connection("http://localhost:8080", "Olivier");
+    //connection.status
+    //    .subscribe(
+    //        status => console.log("Connection status changed to " + status),
+    //        ex => console.log(ex));
+    //connection
+    //    .initialize()
+    //    .subscribe(
+    //        _=> {
+    //            console.log("Connected");
+    //            var refData = new ReferenceDataServiceClient(connection);
+    //            refData.getCurrencyPairUpdates()
+    //                .subscribe(
+    //                    currencyPairs=> console.log(currencyPairs),
+    //                    ex=> console.error(ex));
+    //            var pricing = new PricingServiceClient(connection);
+    //            pricing.getSpotStream("EURUSD")
+    //                .subscribe(
+    //                    (price: PriceDto)=> console.log(price.Bid + "/" + price.Ask),
+    //                    ex=> console.error(ex));
+    //        },
+    //        ex=> console.log(ex));
 };
 var Direction;
 (function (Direction) {
@@ -616,6 +451,35 @@ var PricingServiceClient = (function () {
     };
     return PricingServiceClient;
 })();
+var ReferenceDataServiceClient = (function () {
+    function ReferenceDataServiceClient(connection) {
+        this._connection = connection;
+    }
+    ReferenceDataServiceClient.prototype.getCurrencyPairUpdates = function () {
+        return this.getTradesForConnection(this._connection.referenceDataHubProxy);
+    };
+
+    ReferenceDataServiceClient.prototype.getTradesForConnection = function (referenceDataHubProxy) {
+        var _this = this;
+        return Rx.Observable.create(function (observer) {
+            var currencyPairUpdateSubscription = _this._connection.currencyPairUpdates.subscribe(function (currencyPairUpdate) {
+                return observer.onNext([currencyPairUpdate]);
+            });
+
+            console.log("Sending currency pair subscription...");
+
+            referenceDataHubProxy.invoke("GetCurrencyPairs").done(function (currencyPairs) {
+                observer.onNext(currencyPairs);
+                console.log("Subscribed to currency pairs and received " + currencyPairs.length + " currency pairs.");
+            }).fail(function (ex) {
+                return observer.onError(ex);
+            });
+
+            return currencyPairUpdateSubscription;
+        }).publish().refCount();
+    };
+    return ReferenceDataServiceClient;
+})();
 var ConnectionStatus;
 (function (ConnectionStatus) {
     ConnectionStatus[ConnectionStatus["Connecting"] = 0] = "Connecting";
@@ -635,5 +499,152 @@ var ConnectionInfo = (function () {
         return "ConnectionStatus: " + this.connectionStatus + ", Server: " + this.server;
     };
     return ConnectionInfo;
+})();
+var Connection = (function () {
+    function Connection(address, username) {
+        var _this = this;
+        this._status = new Rx.Subject();
+        this._address = address;
+        this._hubConnection = $.hubConnection("http://localhost:8080/signalr");
+
+        // TODO set username header
+        this._hubConnection.disconnected(function () {
+            return _this.changeStatus(5 /* Closed */);
+        }).connectionSlow(function () {
+            return _this.changeStatus(2 /* ConnectionSlow */);
+        }).reconnected(function () {
+            return _this.changeStatus(4 /* Reconnected */);
+        }).reconnecting(function () {
+            return _this.changeStatus(3 /* Reconnecting */);
+        }).error(function (error) {
+            return console.log(error);
+        });
+
+        this._referenceDataHubProxy = this._hubConnection.createHubProxy("ReferenceDataHub");
+        this._blotterHubProxy = this._hubConnection.createHubProxy("BlotterHub");
+        this._executionHubProxy = this._hubConnection.createHubProxy("ExecutionHub");
+        this._pricingHubProxy = this._hubConnection.createHubProxy("PricingHub");
+
+        this.installListeners();
+    }
+    Connection.prototype.initialize = function () {
+        var _this = this;
+        return Rx.Observable.create(function (observer) {
+            _this.changeStatus(0 /* Connecting */);
+
+            console.log("Connecting to " + _this._address + "...");
+            _this._hubConnection.start().done(function () {
+                _this.changeStatus(1 /* Connected */);
+                observer.onNext(true);
+                console.log("Connected to " + _this._address + ".");
+            }).fail(function () {
+                _this.changeStatus(5 /* Closed */);
+                var error = "An error occured when starting SignalR connection.";
+                console.log(error);
+                observer.onError(error);
+            });
+
+            return Rx.Disposable.create(function () {
+                console.log("Stoping connection...");
+                _this._hubConnection.stop();
+                console.log("Connection stopped.");
+            });
+        });
+        //.publish()
+        //.refCount();
+    };
+
+    Connection.prototype.changeStatus = function (newStatus) {
+        this._status.onNext(new ConnectionInfo(newStatus, this.address));
+    };
+
+    Object.defineProperty(Connection.prototype, "status", {
+        get: function () {
+            return this._status;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Connection.prototype, "address", {
+        get: function () {
+            return this._address;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Connection.prototype, "referenceDataHubProxy", {
+        get: function () {
+            return this._referenceDataHubProxy;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Connection.prototype, "pricingHubProxy", {
+        get: function () {
+            return this._pricingHubProxy;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Connection.prototype, "executionHubProxy", {
+        get: function () {
+            return this._executionHubProxy;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Connection.prototype, "blotterHubProxy", {
+        get: function () {
+            return this._blotterHubProxy;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Connection.prototype, "allPrices", {
+        get: function () {
+            return this._allPrices;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Connection.prototype, "currencyPairUpdates", {
+        get: function () {
+            return this._currencyPairUpdates;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Connection.prototype, "allTrades", {
+        get: function () {
+            return this._allTrades;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Connection.prototype.installListeners = function () {
+        var _this = this;
+        this._allPrices = new Rx.Subject();
+        this._currencyPairUpdates = new Rx.Subject();
+
+        this._pricingHubProxy.on("OnNewPrice", function (price) {
+            return _this._allPrices.onNext(price);
+        });
+        this._referenceDataHubProxy.on("OnCurrencyPairUpdate", function (currencyPairs) {
+            return _this._currencyPairUpdates.onNext(currencyPairs);
+        });
+        this._blotterHubProxy.on("OnNewTrade", function (trades) {
+            return _this._allTrades.onNext(trades);
+        });
+    };
+    return Connection;
 })();
 //# sourceMappingURL=reactiveTrader.js.map
